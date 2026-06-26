448463be0926b3e775ef4520aaba69e9ee470b20
hostplacaresycocinas <hostplacaresycocinas@gmail.com>
2026-06-26 16:25:10 -0300

    Update constants.tsx

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -17,7 +17,7 @@ export const company = {
   googlemapsLink: 'https://maps.app.goo.gl/AiMtsEh1rLktqdCaA',
   googlemaps:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.5133607997764!2d-58.47951372307988!3d-34.616465172949674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc90047a10277%3A0xb13be49a62813dc0!2sArgemotors!5e0!3m2!1ses-419!2sar!4v1763065454996!5m2!1ses-419!2sar',
-  openDays: ['Lunes a Viernes 09:00 a 18:00hs', 'Sábado 09:00 a 13:00hs'],
+  openDays: ['Lunes a Viernes 09:30 a 19:00hs', 'Sábado 10:00 a 17:00hs'],
   footer:
     'Compra y Venta de Autos: Vehículos seleccionados, atención personalizada.',
 };
@@ -29,7 +29,7 @@ export const sedes = [
     adress: 'Av. Nazca 1487',
     city: 'Villa Del Parque',
     image: '/assets/sedes/sede-1.webp',
-    schedule: ['Lunes a Viernes 09:00 a 18:00hs', 'Sábado 09:00 a 13:00hs'],
+    schedule: ['Lunes a Viernes 09:30 a 19:00hs', 'Sábado 10:00 a 17:00hs'],
     googlemapsLink: 'https://maps.app.goo.gl/AiMtsEh1rLktqdCaA',
   },
   {
